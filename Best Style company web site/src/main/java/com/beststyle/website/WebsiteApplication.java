d7780b55f270f588f8fabd10ddd768cf9dd504ca
markov-r <37890942+markov-r@users.noreply.github.com>
2018-07-23 18:38:07 +0300

    Initial Spring/Gradle project.

---
+++ Best Style company web site/src/main/java/com/beststyle/website/WebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.beststyle.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteApplication.class, args);
+    }
+}
